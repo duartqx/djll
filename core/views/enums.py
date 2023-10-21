87745d813d42b83c708603cc20be6a73a425c61f
Diego Duarte <duartqx@gmail.com>
2023-10-21 16:33:41 -0300

    Revert "alertmessage enum"
    This reverts commit 3da537ea4a214ac022b415d42fa3f59bec7acb8f.

--- core/views/enums.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-class AlertMessage(StrEnum):
-    SOMETHING_WENT_WRONG = "Something Went Wrong"
-    ACCOUNT_SUCCESSFULLY_CREATED = "Your account was sucessfully created!"
-
-class AlertBootstrapClass(StrEnum):
-    SUCCESS = "alert-success"
-    DANGER = "alert-danger"
