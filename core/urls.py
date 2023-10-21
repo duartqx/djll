97ac37f28fdfc03603ecc5393ac70f29451ff14f
Diego Duarte <duartqx@gmail.com>
2023-10-21 02:06:44 -0300

    core/urls.py update

--- core/urls.py
+++ core/urls.py
@@ -11,35 +11,48 @@ from .views import (
 )
 from .views.htmx import (
     IndexView,
-    UserDeleteHtmxView,
-    UserEditHtmxView,
-    UserCreateHtmxView,
-    PasswordChangeHtmxView,
+    LoginHtmxView,
 )
 
 htmx_routes = [
     path("", IndexView.as_view(), name="index"),
     path(
         "login/",
-        TemplateView.as_view(template_name="forms/login.html"),
+        LoginHtmxView.as_view(),
         name="loginform",
     ),
     path(
         "user/create/",
-        UserCreateHtmxView.as_view(),
+        TemplateView.as_view(template_name="forms/user_create.html"),
         name="user_create_form",
     ),
-    path("user/edit/", UserEditHtmxView.as_view(), name="user_edit_form"),
-    path("user/delete/", UserDeleteHtmxView.as_view(), name="user_delete_form"),
+    path(
+        "user/edit/",
+        TemplateView.as_view(template_name="forms/user_edit.html"),
+        name="user_edit_form"
+    ),
+    path(
+        "user/delete/",
+        TemplateView.as_view(template_name="forms/user_delete.html"),
+        name="user_delete_form"
+    ),
     path(
         "user/password/",
-        PasswordChangeHtmxView.as_view(),
+        TemplateView.as_view(template_name="forms/password_change.html"),
         name="change_password_form",
+    ),
+
+    # Uses Serializer to post, but returns html response
+    path("api/login/htmx", LoginView.as_view({"post": "htmx_login"}), name="htmx_login"),
+    path(
+        "user/create/htmx/",
+        CreateUserView.as_view({"post": "htmx_create"}),
+        name="createuser_htmx",
     ),
 ]
 
 api_routes = [
-    path("api/login/", LoginView.as_view(), name="login"),
+    path("api/login/", LoginView.as_view({"post": "login"}), name="login"),
     path("api/logout/", LogoutView.as_view(), name="logout"),
     path(
         "api/password/",
