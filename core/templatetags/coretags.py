29f374afde6c2256a788f2c531775651230e9703
Diego Duarte <duartqx@gmail.com>
2023-10-21 04:09:55 -0300

    coretages.py update

--- core/templatetags/coretags.py
+++ core/templatetags/coretags.py
@@ -6,15 +6,23 @@ from core.service.enckeys import EncryptionService
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True)
-def encryption_key(context):
-    session = context["request"].session
-
-    encryption_key = EncryptionService.get_keys(session).get("enckey")
+def get_key(session) -> str:
+    encryption_key = EncryptionService.get_keys(session).get("enckey", "")
 
     if session.get("encryption_key") is None:
         session["encryption_key"] = encryption_key
 
+    return encryption_key
+
+
+@register.simple_tag(takes_context=True)
+def encryption_key(context):
+    enckey = get_key(context["request"].session)
     return mark_safe(
-        f"""<input type="hidden" name="enckey" value="{encryption_key}"></input>"""
+        f"""<input type="hidden" name="enckey" value="{enckey}"></input>"""
     )
+
+
+@register.simple_tag(takes_context=True)
+def get_encryption_key(context):
+    return get_key(context["request"].session)
