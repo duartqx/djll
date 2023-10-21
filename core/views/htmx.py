bd41324ac45f2f1f775e6f06138a9248657c4431
Diego Duarte <duartqx@gmail.com>
2023-10-21 02:05:51 -0300

    Removed most of htmx.py views and used hx-select instead

--- core/views/htmx.py
+++ core/views/htmx.py
@@ -8,33 +8,19 @@ class IndexView(TemplateView):
 
     def dispatch(self, request, *args, **kwargs):
         if not self.request.user.is_authenticated:
+            if request.GET:
+                params = "?"
+                for key, value in request.GET.items():
+                    params += f"{key}={value}"
+                return HttpResponseRedirect(reverse("loginform") + params)
             return HttpResponseRedirect(reverse("loginform"))
         return super().dispatch(request, *args, **kwargs)
 
 
-class UserCreateHtmxView(TemplateView):
-    def get_template_names(self):
-        if self.request.headers.get("HX-Request"):
-            return ["forms/partials/_partial_user_create.html"]
-        return ["forms/user_create.html"]
+class LoginHtmxView(TemplateView):
+    template_name = "forms/login.html"
 
-
-class UserDeleteHtmxView(TemplateView):
-    def get_template_names(self):
-        if self.request.headers.get("HX-Request"):
-            return ["forms/partials/_partial_user_delete.html"]
-        return ["forms/user_delete.html"]
-
-
-class UserEditHtmxView(TemplateView):
-    def get_template_names(self):
-        if self.request.headers.get("HX-Request"):
-            return ["forms/partials/_partial_user_edit.html"]
-        return ["forms/user_edit.html"]
-
-
-class PasswordChangeHtmxView(TemplateView):
-    def get_template_names(self):
-        if self.request.headers.get("HX-Request"):
-            return ["forms/partials/_partial_password_change.html"]
-        return ["forms/password_change.html"]
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("index"))
+        return super().dispatch(request, *args, **kwargs)
