90cdba996bdae62539212a87a2410f8276c64abc
Diego Duarte <duartqx@gmail.com>
2023-10-21 23:24:48 -0300

    Encapsulated coretags tags to EncryptionKeyTag class

--- core/templatetags/coretags.py
+++ core/templatetags/coretags.py
@@ -1,28 +1,46 @@
 from django import template
 from django.utils.safestring import mark_safe
 
-from ..service.encryption import EncryptionService
+from ..service.encryption.fernet import FernetEncryptionSenvice
 
 register = template.Library()
 
 
-def get_key(session) -> str:
-    encryption_key = EncryptionService(session).get_enckey()
+class EncryptionKeyTag:
+    service = FernetEncryptionSenvice
 
-    if session.get("encryption_key") is None:
-        session["encryption_key"] = encryption_key
+    @classmethod
+    def get_key(cls, session) -> str:
+        encryption_key = cls.service(session).get_enckey()
 
-    return encryption_key
+        if session.get("encryption_key") is None:
+            session["encryption_key"] = encryption_key
+
+        return encryption_key
+
+    @classmethod
+    def encryption_key_wrapper(cls):
+        @register.simple_tag(takes_context=True)
+        def encryption_key(context):
+            enckey = cls.get_key(context["request"].session)
+            return mark_safe(
+                f"""<input type="hidden" name="enckey" value="{enckey}"></input>"""
+            )
+
+        return encryption_key
+
+    @classmethod
+    def get_encryption_key_wrapper(cls):
+        @register.simple_tag(takes_context=True)
+        def get_encryption_key(context):
+            return cls.get_key(context["request"].session)
+
+        return get_encryption_key
+
+    @classmethod
+    def build(cls):
+        cls.get_encryption_key_wrapper()
+        cls.encryption_key_wrapper()
 
 
-@register.simple_tag(takes_context=True)
-def encryption_key(context):
-    enckey = get_key(context["request"].session)
-    return mark_safe(
-        f"""<input type="hidden" name="enckey" value="{enckey}"></input>"""
-    )
-
-
-@register.simple_tag(takes_context=True)
-def get_encryption_key(context):
-    return get_key(context["request"].session)
+EncryptionKeyTag.build()
