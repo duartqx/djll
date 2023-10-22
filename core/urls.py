dd2e49d23d6a72e7d8bbf31cdcb17f707e5ccda7
Diego Duarte <duartqx@gmail.com>
2023-10-21 23:42:37 -0300

    Replaced TemplateView with ChangePasswordHtmxView

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,7 @@ from .views import (
     TokensView,
 )
 from .views.htmx import (
+    ChangePasswordHtmxView,
     IndexView,
     LoginHtmxView,
     CreateUserHtmxView,
@@ -20,37 +21,37 @@ htmx_routes = [
     path(
         "login/",
         LoginHtmxView.as_view({"get": "get", "post": "login"}),
-        name="loginform",
+        name="login",
     ),
     path(
         "user/create/",
         CreateUserHtmxView.as_view({"get": "get", "post": "create"}),
-        name="user_create_form",
+        name="user_create",
     ),
     path(
         "user/edit/",
         TemplateView.as_view(template_name="forms/user_edit.html"),
-        name="user_edit_form",
+        name="user_edit",
     ),
     path(
         "user/delete/",
         TemplateView.as_view(template_name="forms/user_delete.html"),
-        name="user_delete_form",
+        name="user_delete",
     ),
     path(
         "user/password/",
-        TemplateView.as_view(template_name="forms/password_change.html"),
-        name="change_password_form",
+        ChangePasswordHtmxView.as_view({"get": "get", "patch": "partial_update"}),
+        name="change_password",
     ),
 ]
 
 api_routes = [
-    path("api/login/", LoginView.as_view({"post": "login"}), name="login"),
-    path("api/logout/", LogoutView.as_view(), name="logout"),
+    path("api/login/", LoginView.as_view({"post": "login"}), name="api_login"),
+    path("api/logout/", LogoutView.as_view(), name="api_logout"),
     path(
         "api/password/",
         ChangePasswordView.as_view({"patch": "partial_update"}),
-        name="password",
+        name="api_password",
     ),
     path(
         "api/user/",
@@ -61,14 +62,14 @@ api_routes = [
                 "patch": "partial_update",
             }
         ),
-        name="user",
+        name="api_user",
     ),
     path(
         "api/user/create/",
         CreateUserView.as_view({"post": "create"}),
-        name="createuser",
+        name="api_createuser",
     ),
-    path("api/tokens/", TokensView.as_view(), name="tokens"),
+    path("api/tokens/", TokensView.as_view(), name="api_tokens"),
 ]
 
 urlpatterns = htmx_routes + api_routes
